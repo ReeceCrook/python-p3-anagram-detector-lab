3bd2ce900c037e5ecb8ca7c5a0cf391a6c3fb61b
Reece Crook <reececrook16@gmail.com>
2023-08-08 02:22:30 -0600

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def get_word(self):
+        return self._word
+    
+    def set_word(self, word):
+        self._word = word
+
+    def match(self, list):
+        anagram_list = []
+        for i in range(len(list)):
+            if sorted([letter for letter in self.word]) == sorted([letter for letter in list[i]]):
+                anagram_list.append(list[i])
+        return anagram_list
+    word = property(get_word, set_word)
+
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
